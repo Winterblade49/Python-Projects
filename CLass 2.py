71aa0eb544cb3bb4c93167ca00c930da7117d9ad
Winterblade49 <Winterblade49@gmail.com>
2023-07-14 00:08:30 -0600

    Update CLass 2.py

--- CLass 2.py
+++ CLass 2.py
@@ -1,16 +1,17 @@
-class User:
-    name = 'no name provided'
-    email = ' '
-    password = '1234abcd'
-    account_number = 0
+#parent class user 
+
+class User: #class A
+    name = "Mark"
+    email = "mark@gmail.com"
+    password = "123abcd"
     
-class Employee(User):
-    Base_pay = 11.00
-    deparment = 'general'
-    
-class Customer(User):
-    mailing_adress = ''
-    mailing_list = True
-    
+    def getLoginInfo(self): #as instance
+        entry_name = input("Enter Your name: ")
+        entry_email = input("Enter your email")
+        entry_password = input("Enter your password")
+        if(entry_email == self.email and entry_password == self.password)
+            
+                
+
 
     
